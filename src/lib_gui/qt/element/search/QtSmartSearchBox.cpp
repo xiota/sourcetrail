7fb07db63147ed6023c6d97aa2884cd456712cdb
xiota <aur@mentalfossa.com>
2024-06-22 05:49:58 +0000

    fix: switch from abs to std::abs

--- src/lib_gui/qt/element/search/QtSmartSearchBox.cpp
+++ src/lib_gui/qt/element/search/QtSmartSearchBox.cpp
@@ -551,7 +551,7 @@ void QtSmartSearchBox::mouseReleaseEvent(QMouseEvent* event)
 
 	m_mousePressed = false;
 
-	if (abs(event->x() - m_mouseX) > 5)
+	if (std::abs(event->x() - m_mouseX) > 5)
 	{
 		return;
 	}
@@ -561,7 +561,7 @@ void QtSmartSearchBox::mouseReleaseEvent(QMouseEvent* event)
 	for (size_t i = 0; i < m_elements.size(); i++)
 	{
 		int dist = m_elements[i]->x() + m_elements[i]->width() - event->x();
-		if (abs(dist) < abs(minDist))
+		if (std::abs(dist) < std::abs(minDist))
 		{
 			pos = static_cast<int>(i) + 1;
 			minDist = dist;
